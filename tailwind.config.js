03fc07d483a4a950b1c15a0267fe325cb6070dcd
Tiago Tavares <bigtavadev@gmail.com>
2023-05-04 00:28:11 +0100

    refactor: removing uncessary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
